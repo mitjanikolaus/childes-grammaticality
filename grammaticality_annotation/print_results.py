bce1c2650d216e99b1f683f634f6639f48efc7f8
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2023-08-11 15:28:33 +0200

    attention layer for lstm

--- grammaticality_annotation/print_results.py
+++ grammaticality_annotation/print_results.py
@@ -1,12 +1,34 @@
 import pandas as pd
 from utils import RESULTS_FILE
 
+REFERENCE_METRIC = "val_mcc: mean"
+
+
+def create_results_table_model_comparison(results, context_length=1):
+    results_context_length = results[results.context_length == context_length].copy()
+
+    print(results_context_length.to_markdown(index=False, floatfmt=".2f"))
+    # print("\n\n\n")
+    # print(results.to_latex(float_format="%.2f", index=False))
+
+
+def create_results_table_context_lengths(results, model="roberta-large"):
+    results_model = results[results.model == model].copy()
+    print(results_model.to_markdown(index=False, floatfmt=".2f"))
+    # print("\n\n\n")
+    # print(results.to_latex(float_format="%.2f", index=False))
+
+    best_context_length = results_model.sort_values(REFERENCE_METRIC, ascending=False).iloc[0].context_length
+    print(f"\nBest context length: {best_context_length}\n")
+    return best_context_length
+
 
 def main():
     results = pd.read_csv(RESULTS_FILE)
-    print(results.to_markdown(index=False, floatfmt=".2f"))
-    print("\n\n\n")
-    print(results.to_latex(float_format="%.2f", index=False))
+
+    best_context_length = create_results_table_context_lengths(results)
+
+    create_results_table_model_comparison(results, best_context_length)
 
 
 if __name__ == "__main__":
