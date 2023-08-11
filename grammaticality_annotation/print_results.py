3e94ff32658e83143144c69516d0621237bea237
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2023-08-11 17:07:17 +0200

    update print_results.py

--- grammaticality_annotation/print_results.py
+++ grammaticality_annotation/print_results.py
@@ -5,6 +5,8 @@ REFERENCE_METRIC = "val_mcc: mean"
 
 
 def create_results_table_model_comparison(results, context_length=1):
+    print("\n\nMODEL COMPARISON:")
+
     results_context_length = results[results.context_length == context_length].copy()
 
     print(results_context_length.to_markdown(index=False, floatfmt=".2f"))
@@ -13,6 +15,8 @@ def create_results_table_model_comparison(results, context_length=1):
 
 
 def create_results_table_context_lengths(results, model="roberta-large"):
+    print("\n\nEXP CONTEXT LENGTHS:")
+
     results_model = results[results.model == model].copy()
     print(results_model.to_markdown(index=False, floatfmt=".2f"))
     # print("\n\n\n")
@@ -29,6 +33,10 @@ def main():
     best_context_length = create_results_table_context_lengths(results)
 
     create_results_table_model_comparison(results, best_context_length)
+
+    print("\n\nALL RESULTS:")
+    print(results.to_markdown(index=False, floatfmt=".2f"))
+
 
 
 if __name__ == "__main__":
