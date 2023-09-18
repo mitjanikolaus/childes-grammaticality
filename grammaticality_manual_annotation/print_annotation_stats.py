2ce795b650838daa77896fa0e6d591ede5e6b9a9
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2023-09-18 15:46:09 +0200

    replace pie chart with bar chart

--- grammaticality_manual_annotation/print_annotation_stats.py
+++ grammaticality_manual_annotation/print_annotation_stats.py
@@ -15,10 +15,6 @@ from utils import RESULTS_DIR
 #     )
 
 
-COLORS_PLOT_CATEGORICAL = sns.color_palette("Set2")
-COLORS_PLOT_CATEGORICAL.extend(["#FF4A46", "#997D87", "#d402bf", "#1CE6FF"])
-
-
 def age_bin(age, num_months=12):
     return int((age + num_months / 2) / num_months) * num_months
 
@@ -37,15 +33,21 @@ def main():
     data_ungrammatical[ERROR_LABELS_FIELD] = data_ungrammatical[ERROR_LABELS_FIELD].apply(lambda x: x.split(", "))
     data_ungrammatical = data_ungrammatical.explode(ERROR_LABELS_FIELD)
 
-    matplotlib.rcParams.update({'font.size': 15})
     counts = data_ungrammatical[ERROR_LABELS_FIELD].value_counts()
-    counts.plot(kind="pie", colors=COLORS_PLOT_CATEGORICAL)
+    plt.figure(figsize=(4, 5))
+    ax = counts.plot(kind="barh", color="#617fab")
+    percentages = data_ungrammatical[ERROR_LABELS_FIELD].value_counts(normalize=True) * 100
+    patches = ax.patches
+    for i in range(len(patches)):
+        x = patches[i].get_width() + 5
+        y = patches[i].get_y() + 0.1
+        ax.annotate(f"{percentages[i]:.1f}%", (x, y), ha='left')
+    plt.xlim((0, 440))
+    plt.xlabel("Number of errors")
     plt.tight_layout()
-    plt.ylabel("")
     plt.savefig(os.path.join(RESULTS_DIR, "manual_annotation_error_labels.png"), dpi=300)
 
     plt.show()
-
 
     # data["age"] = data.age.apply(age_bin)
     # data.dropna(subset="is_grammatical", inplace=True)
