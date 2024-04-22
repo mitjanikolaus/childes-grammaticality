1d24aa7d0e3a4a393fb81c5ad20e2e2beb174fdf
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-04-22 16:18:38 +0200

    add childes-db metadata info tables

--- grammaticality_annotation/annotate_grammaticality_nn.py
+++ grammaticality_annotation/annotate_grammaticality_nn.py
@@ -11,18 +11,20 @@ import pandas as pd
 
 from grammaticality_annotation.data import CHILDESGrammarDataModule, add_context
 from grammaticality_annotation.fine_tune_grammaticality_nn import CHILDESGrammarModel
-from load_childes_db_data import DATA_DIR_PREPROCESSED_CHILDES_DB
+from load_childes_db_data import DATA_DIR_PREPROCESSED_CHILDES_DB, DB_VERSION
 from utils import PROJECT_ROOT_DIR
 
-ANNOTATION_ANNOTATED_FILES_PATH = PROJECT_ROOT_DIR+"/data/manual_annotation/automatically_annotated"
+ANNOTATION_ANNOTATED_FILES_PATH = PROJECT_ROOT_DIR + "/data/manual_annotation/automatically_annotated"
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Needs to match the number of utterances within a file to be annotated!
-BATCH_SIZE = 200 #TODO
+BATCH_SIZE = 200
 
 DATA_DIR_ANNOTATED = os.path.join(PROJECT_ROOT_DIR, "data", "automatically_annotated", "childes_db")
 DATA_FILE_ANNOTATED_ALL = os.path.join(PROJECT_ROOT_DIR, "data", "automatically_annotated", "childes_db.csv")
+DATA_FILE_CHILDES_DB_METADATA = os.path.join(PROJECT_ROOT_DIR, "data", "automatically_annotated", "metadata.csv")
+DATA_FILE_CHILDES_DB_VARIABLES = os.path.join(PROJECT_ROOT_DIR, "data", "automatically_annotated", "variables.csv")
 
 
 def annotate(args):
@@ -53,7 +55,7 @@ def annotate(args):
                                   train_data_size=1,
                                   ds_dict=dataset_dict)
 
-    checkpoints = list(glob.glob(args.model+"/checkpoints/epoch*.ckpt"))
+    checkpoints = list(glob.glob(args.model + "/checkpoints/epoch*.ckpt"))
     assert len(checkpoints) == 1, "No or multiple checkpoints found."
     checkpoint = checkpoints[0]
     print(f"Model checkpoint: {checkpoint}")
@@ -85,7 +87,7 @@ def annotate(args):
     data_annotated.dropna(inplace=True)
 
     print(f"saving {len(data_annotated)} utterances")
-    data_annotated.to_csv(args.out_data_file, index=False)
+    data_annotated.to_csv(DATA_FILE_ANNOTATED_ALL, index=False)
 
 
 def parse_args():
@@ -101,9 +103,8 @@ def parse_args():
         default=DATA_DIR_ANNOTATED,
     )
     argparser.add_argument(
-        "--out-data-file",
-        type=str,
-        default=DATA_FILE_ANNOTATED_ALL,
+        "--childes-db-export",
+        action="store_true",
     )
     argparser.add_argument(
         "--model",
@@ -123,5 +124,25 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
+    metadata = pd.DataFrame.from_records([{
+        "dataset_name": "grammaticality_deberta",
+        "entity_type": "utterances",
+        "childes_db_version": DB_VERSION,
+        "dataset_version": "1",
+        "tag_type": "model",
+        "model_version": "1",
+        "date_of_release": "2024-05-01",
+        "contact": "mitja.nikolaus@posteo.de",
+        "citation": "https://doi.org/10.48550/arXiv.2403.14208",
+    }])
+    metadata.to_csv(DATA_FILE_CHILDES_DB_METADATA, index=False)
 
-    annotate(args)
+    variables_meta = pd.DataFrame.from_records([{
+        "variable_id": "1",
+        "variable_name": "is_grammatical",
+        "data_type": "float",
+        "values": "[-1,0,1]",
+    }])
+    variables_meta.to_csv(DATA_FILE_CHILDES_DB_VARIABLES, index=False)
+
+    # annotate(args)
